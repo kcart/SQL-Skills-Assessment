1f0eed67f6820ff8df3e07dbcd0e2cb772cfe40c
Hackbright Student <info@hackbrightacademy.com>
2015-05-02 21:01:30 -0700

    committing final response.

--- skills.sql
+++ skills.sql
@@ -1,30 +1,36 @@
 -- 1. Select all columns for all brands in the Brands table.
-
+    SELECT * FROM Brands;
 -- 2. Select all columns for all car models made by Pontiac in the Models table.
-
+    SELECT * FROM Models WHERE brand_name = "Pontiac";
 -- 3. Select the brand name and model 
 --    name for all models made in 1964 from the Models table.
-
+    SELECT brand_name, name FROM Models WHERE year = "1964";
 
 -- 4. Select the model name, brand name, and headquarters for the Ford Mustang 
 --    from the Models and Brands tables.
+    SELECT m.name, brand_name,  headquarters FROM Models AS m LEFT JOIN Brands AS b on m.brand_name = b.name WHERE m.name = "Mustang";
 
 -- 5. Select all rows for the three oldest brands 
 --    from the Brands table (Hint: you can use LIMIT and ORDER BY).
+    SELECT * FROM Brands ORDER BY founded LIMIT 3;
 
 -- 6. Count the Ford models in the database (output should be a **number**).
+    SELECT COUNT(*) FROM Models as M INNER JOIN Brands AS b ON b.name = m.brand_name WHERE b.name = 'Ford';
 
 -- 7. Select the **name** of any and all car brands that are not discontinued.
+    SELECT b.name, m.brand_name FROM Models AS m JOIN Brands AS b WHERE b.name <> b.discontinued; 
 
 -- 8. Select rows 15-25 of the DB in alphabetical order by model name.
+  I am havig some trouble getting the correct output, but I got this- which is somewhat close. What should I be doing?
+  SELECT * FROM Models AS m JOIN Brands AS b ORDER BY b.name LIMIT 10 OFFSET 14;
 
 -- 9. Select the **brand, name, and year the model's brand was 
 --    founded** for all of the models from 1960. Include row(s)
 --    for model(s) even if its brand is not in the Brands table.
 --    (The year the brand was founded should be ``null`` if 
 --    the brand is not in the Brands table.)
-
-
+    SELECT b.name, m.brand_name, b.founded FROM Models AS m JOIN Brands AS b WHERE b.founded = 1960 AND b.founded = 'null';
+  
 
 -- Part 2: Change the following queries according to the specifications. 
 -- Include the answers to the follow up questions in a comment below your
@@ -40,6 +46,7 @@
     --   LEFT JOIN brands AS b
     --     ON b.name = m.brand_name
     -- WHERE b.discontinued IS NULL;
+SELECT b.name, b.founded, m.name FROM Models AS m LEFT JOIN brands AS b ON b.name = m.brand_name WHERE b.discontinued is NULL AND m.brand_name is NULL;
 
 -- 2. Modify this left join so it only selects models that have brands in the Brands table.
 -- before: 
@@ -52,6 +59,9 @@
 
 -- followup question: In your own words, describe the difference between 
 -- left joins and inner joins.
+SELECT m.name, m.brand_name, b.founded FROM Models AS m LEFT JOIN Brands AS b ON b.name = m.brand_name GROUP BY m.name HAVING b.name;
+Had some trouble with this one as well. It didnt produce anything.
+Inner JOIN only shows results that match both table. Left Join shows all overlap, and all the information from the first table. 
 
 -- 3. Modify the query so that it only selects brands that don't have any car models in the cars table. 
 -- (Hint: it should only show Tesla's row.)
@@ -62,6 +72,8 @@
     --   LEFT JOIN Models
     --     ON brands.name = Models.brand_name
     -- WHERE Models.year > 1940;
+    SELECT m.name, b.name, b.founded FROM Brands AS b LEFT JOIN Models AS m ON b.name = m.brand_name WHERE m.name is NULL;
+
 
 -- 4. Modify the query to add another column to the results to show 
 -- the number of years from the year of the model *until* the brand becomes discontinued
@@ -75,6 +87,7 @@
     --   LEFT JOIN brands AS b
     --     ON m.brand_name = b.name
     -- WHERE b.discontinued NOT NULL;
+ SELECT b.name, m.name, m.year, b.discontinued, (b.discontinued -m.year) AS years_until_brand_discontinued FROM Models AS m LEFT JOIN brands AS b ON m.brand_name = b.name WHERE b.discontinued NOT NULL;
 
 
 
